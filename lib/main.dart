6e6bef15559190f0d9f8855989673f08c79e015e
IbrahimAlsaadany <ganlogepo21@gmail.com>
2024-05-06 18:37:34 +0300

    Validation & Submission

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ml_project/Project.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 class MyApp extends StatefulWidget{
   const MyApp({super.key});
